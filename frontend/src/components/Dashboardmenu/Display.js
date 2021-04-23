7e7af9f0abeb926553a1fcedfae3c2c15e2bc05d
Maria Mohl <mars381pika@gmail.com>
2021-04-23 17:10:18 -0400

    Display and Listing components are successfully reusable

--- frontend/src/components/Dashboardmenu/Display.js
+++ frontend/src/components/Dashboardmenu/Display.js
@@ -11,14 +11,10 @@ const Display = (props) =>{
 
 		{props.products.filter(product => product._id === props.partId).map(filteredPart => (
 				<>
-				<p>Part # {filteredPart._id}</p>
-				<p>Name: {filteredPart.name}</p>
-				<p>Brand: {filteredPart.brand}</p>
-				<p>Series: {filteredPart.series}</p>
-				<p>Input Voltage: {filteredPart.input_volt_min} - {filteredPart.input_volt_max}</p>
-				<p>Type: {filteredPart.type}</p>
-				<p>Maximum Power: {filteredPart.max_power}</p>
-				<p>Maximum Length: {filteredPart.max_length}mm</p>
+				{
+					Object.keys(filteredPart).map((value, index) => {
+						return <p>{value} : {filteredPart[Object.keys(filteredPart)[index]]}</p>
+					})}
 				</>
 			))}
 
